ca6833d0c0a4c1d9cad619798edfbac83c142c1a
Assert Inequality <assert.inequality@proton.me>
2023-09-05 14:15:27 +0300

    Add completion and pairs to dev plugins

--- config/plugins/dev.nix
+++ config/plugins/dev.nix
@@ -29,6 +29,64 @@
 			};
 		};
 
+		nvim-cmp = {
+			enable = true;
+			mapping = {
+				"<C-n>" = "cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert })";
+				"<C-p>" = "cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert })";
+				"<C-b>" = "cmp.mapping.scroll_docs(-4)";
+				"<C-f>" = "cmp.mapping.scroll_docs(4)";
+				"<C-Space>" = "cmp.mapping.complete()";
+				"<C-e>" = "cmp.mapping.abort()";
+				"<CR>" = "cmp.mapping.confirm({ select = true })";
+				"<S-CR>" = ''cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+						})'';
+			};
+			sources = [
+			{ name = "nvim_lsp"; }
+			{ name = "luasnip"; }
+			{ name = "buffer"; }
+			{ name = "path"; }
+			];
+		};
+
+		cmp-nvim-lsp.enable = true;
+		cmp-buffer.enable = true;
+		cmp-path.enable = true;
+		cmp_luasnip.enable = true;
+
+		ts-context-commentstring.enable = true;
+
+		mini = {
+			enable = true;
+			modules = {
+				pairs = { };
+				comment = { };
+				surround = {
+					add = "gza"; 
+					delete = "gzd"; 
+					find = "gzf"; 
+					find_left = "gzF"; 
+					highlight = "gzh"; 
+					replace = "gzr"; 
+					update_n_lines = "gzn";
+				};
+				ai = {
+					n_lines = 500;
+					custom_textobjects = {
+						o = ''ai.gen_spec.treesitter({
+								a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+								i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+								}, {}),'';
+						f = ''ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),'';
+						c = ''ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),'';
+					};
+				};
+			};
+		};
+
 
 	};
 }
