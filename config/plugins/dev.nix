7a387626a35bf98a0fde487d3bbac35dbd100aaf
Assert Inequality <assert.inequality@proton.me>
2023-09-05 20:43:09 +0300

    Add mini.comment ts-context function

--- config/plugins/dev.nix
+++ config/plugins/dev.nix
@@ -63,7 +63,13 @@
 			enable = true;
 			modules = {
 				pairs = { };
-				comment = { };
+				comment = {
+					options = {
+						custom_commentstring.__raw = ''function()
+						return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+						end,'';
+					};
+				};
 				surround = {
 					add = "gza"; 
 					delete = "gzd"; 
