2ee18a828c180cc6dd880a16698cdd4992413664
AssertInequality <assert.inequality@proton.me>
2024-11-20 17:23:42 +0200

    [Editor] Add indent lines

--- config/plugins/editor.nix
+++ config/plugins/editor.nix
@@ -55,5 +55,30 @@
       };
     };
 
+    indent-blankline = {
+      enable = true;
+      settings = {
+        exclude = {
+          buftypes = [
+            "terminal"
+            "quickfix"
+          ];
+          filetypes = [
+            ""
+            "checkhealth"
+            "help"
+            "lspinfo"
+            "packer"
+            "TelescopePrompt"
+            "TelescopeResults"
+            "yaml"
+          ];
+        };
+        indent = {
+          char = "│";
+        };
+      };
+    };
+
   };
 }
