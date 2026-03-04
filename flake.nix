57c15581ef86f34f2073438afd8568c0d61f852d
AssertInequality <assert.inequality@proton.me>
2026-03-04 15:17:46 +0200

    [Update] Bump to nixpkgs 25.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Personal NixVim Config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixvim.url = "github:nix-community/nixvim/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixvim.url = "github:nix-community/nixvim/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
